d28490d1c151f5aea3b127c75e3326db1f8b6f54
aryndraa <aryacoder1102@gmail.com>
2025-02-10 23:09:56 +0800

    Day 7 - use middleware on /account

--- middleware.js
+++ middleware.js
@@ -1,10 +1,15 @@
-import { NextResponse } from "next/server"
+// import { NextResponse } from "next/server"
 
-export function middleware(request){
-  console.log(request)
+// export function middleware(request){
+//   console.log(request)
 
-  return NextResponse.redirect(new URL("/about", request.url))
-}
+//   return NextResponse.redirect(new URL("/about", request.url))
+// }
+
+
+import { auth } from "./app/_lib/auth"
+
+export const middleware= auth;
 
 export const config = {
   matcher: ["/account"]

f425b5ecf6291aa6a11bcdb5396d904f8e8b8ca7
aryndraa <aryacoder1102@gmail.com>
2025-02-10 22:44:12 +0800

    Day 7 - user session for navbar

--- app/_components/Navigation.js
+++ app/_components/Navigation.js
@@ -1,20 +1,59 @@
 import Link from 'next/link'
+import { auth } from "@/app/_lib/auth"
 
-export default function Navigation() {
-    return (
-        <ul className="flex gap-12">
-          <li>
-            <Link href="/">Home</Link>
-          </li>
-          <li>
-            <Link href="/cabins">Cabins</Link>
-          </li>
-          <li>
-            <Link href="/about">About</Link>
-          </li>
-          <li>
-                <Link href="/account">Account</Link>
-            </li>
-        </ul>
-    )
+export default async function Navigation() {
+  const session = await auth();
+  console.log(session)
+  return (
+    <ul className="flex gap-12">
+      <li>
+        <Link
+          href="/" 
+          className='hover:text-accent-400 transition-colors'
+        >
+          Home
+        </Link>
+      </li>
+      <li>
+        <Link
+          href="/cabins" 
+          className='hover:text-accent-400 transition-colors'
+        >
+          Cabins
+        </Link>
+      </li>
+      <li>
+        <Link
+          href="/about" 
+          className='hover:text-accent-400 transition-colors'
+        >
+          About
+        </Link>
+      </li>
+      <li>
+       {session?.user?.image? 
+        <Link
+          href="/account" 
+          className='hover:text-accent-400 transition-colors flex items-center gap-4'
+        >
+          <img 
+            className="h-8 rounded-full" 
+            src={session.user.image}
+            alt={session.user.name}
+            referrerPolicy='no-referrer'
+          />
+          <span>
+           {session.user.name}
+          </span>
+        </Link>
+       :
+        <Link
+          href="/account" 
+          className='hover:text-accent-400 transition-colors'
+        >
+          Guest Area
+        </Link>}
+      </li>
+    </ul>
+  )
 }

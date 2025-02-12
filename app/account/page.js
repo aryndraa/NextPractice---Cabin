26a395daad79ecdac063aebbc3368c720dc2f4a9
aryndraa <aryacoder1102@gmail.com>
2025-02-12 22:30:32 +0800

    Day 8 - use session data

--- app/account/page.js
+++ app/account/page.js
@@ -1,8 +1,13 @@
-export default function Pages() {
+import { auth } from "../_lib/auth";
+
+export default async function Pages() {
+
+  const session = await auth();
+  const firstName = session.user.name.split(" ").at(0);
   return (
     <div>
       <h2 className="font-semibold text-2xl text-accent-400 mb-7">
-        Welcome, Jonas
+        Welcome, {firstName}
       </h2>
     </div>
   );
